6b51721d4cc169d3bac19042ad4bb8963e667110
gyhaan <g.yhaan@alustudent.com>
2024-03-26 19:46:39 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
+
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  ul.innerHTML = "";
+  tasks.forEach(function (task, index) {
+    const html = `<li><input type="checkbox" ${
+      task.status ? "checked" : ""
+    }/><span style="text-decoration: ${task.status ? "line-through" : ""}">${
+      task.name
+    }</span><button>X</button></li>`;
+    ul.insertAdjacentHTML("beforeend", html);
+    ul.lastElementChild
+      .querySelector('input[type="checkbox"]')
+      .addEventListener("change", function () {
+        task.status = !task.status;
+        renderTasks();
+      });
+    ul.lastElementChild
+      .querySelector("button")
+      .addEventListener("click", function () {
+        tasks.splice(index, 1);
+        renderTasks();
+      });
+  });
+}
+
+button.addEventListener("click", function (e) {
+  if (!input.value) return;
+  tasks.push({ name: input.value, status: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+  input.value = "";
+});
+
+renderTasks();
